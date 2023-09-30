a8f6b24da6dbcb643209f70be3dc783f6a4e0513
Gian Zamora <gz2016@att.net>
2023-09-30 14:52:00 -0700

    updated cors policies v5

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ require('dotenv').config();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'https://monchis-world.netlify.app/');
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization'
+	);
 	next();
 });
-
-app.use(cors());
 
 app.post('/password', (req, res) => {
 	const password = req.body.password;
