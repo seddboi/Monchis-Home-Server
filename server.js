ccf6085d1c1101cd42d546ff3f3eccf297d87fb5
Gian Zamora <gz2016@att.net>
2023-09-30 14:31:09 -0700

    updated acces header v2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 app.use(express.json());
 
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+	res.header('Access-Control-Allow-Origin', 'http://localhost:5173/');
 	next();
 });
 
