3167769a8e1e05833c7e01d66479c3159f9934f9
Gian Zamora <gz2016@att.net>
2023-10-03 21:32:32 -0700

    updated access header v7

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const cors = require('cors');
 const { factSpitter } = require('./utils/fact-collection');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 require('dotenv').config();
 
 app.use(express.json());
 
 app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization'
-	);
+	res.header('Access-Control-Allow-Origin', 'https://monchis-world.netlify.app');
+	// res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+	// res.header(
+	// 	'Access-Control-Allow-Headers',
+	// 	'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization'
+	// );
 	next();
 });
 
