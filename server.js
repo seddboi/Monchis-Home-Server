3ab0d36a4796246d057419be1c8cd9f3b711dff9
Gian Zamora <gz2016@att.net>
2023-10-03 22:04:12 -0700

    adjusted access control header and allowed methods

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', 'https://monchis-world.netlify.app');
-	// res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-	// res.header(
-	// 	'Access-Control-Allow-Headers',
-	// 	'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization'
-	// );
+	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization'
+	);
 	next();
 });
 
